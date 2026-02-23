bccf33d87545e6981823833ba45eeba9a0740307
root <root@vmi2955525.contaboserver.net>
2026-02-23 22:09:47 +0100

    analytics

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://umami.technioz.com/script.js',
+      defer: true,
+      'data-website-id': '3a676a6d-3b90-4a60-acb1-6cd8f75d0601'
+    }
+  ]
+})
+</script>
